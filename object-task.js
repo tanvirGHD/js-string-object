51364e2239ca7266b53d4bfa18fea04b2fb4193a
tanvirGHD <155833719+tanvirGHD@users.noreply.github.com>
2024-09-04 10:27:07 +0600

    js-simple-object-task-solve

--- object-task.js
+++ object-task.js
@@ -1,4 +1,5 @@
-//Access the golden rod color value in output.
+//1.Access the golden rod color value in output.
+
 // const colors = {
 //     red: "#ff0000",
 //     green: "#00ff00",
@@ -8,10 +9,59 @@
 // console.log(colors["golden rod"]);
 
 
-// For this object below add a property named passenger capacity with value 5
 
-const car = {
-    make: "Toyota",
-    model: "Corolla",
-    year: 2020
+//2.For this object below add a property named passenger capacity with value 5
+
+// const car = {
+//     make: "Toyota",
+//     model: "Corolla",
+//     year: 2020,
+//     "passenger capacity":5
+// };
+// console.log(car["passenger capacity"]);
+
+
+
+//3.Display the physics marks as output.
+
+// const student = {
+//     name: "Hamim Sakep",
+//     id: 5421,
+//     physics: {
+//         subject: "HSC Physics",
+//         author: "Shahjahan Tapan",
+//         marks: 30
+//     }
+// };
+// console.log(student.physics.marks);
+
+
+
+//4.Count the number of properties.
+
+// let student = {
+//     name: 'Ariana Grande',
+//     age: 21,
+//     city: 'Gaibandha',
+//     isStudent: true
+// };
+// const keys = Object.keys(student);
+// console.log(keys);
+
+
+
+
+
+//5.Loop through an object and print the key-value pairs with their types
+
+let myObject = {
+name: 'John Doe',
+age: 25,
+city: 'Example City',
+isStudent: true
 };
+const keys = Object.keys(myObject);
+for(const key of keys){
+    console.log('key:', key, '|', 'type:', myObject[key]);
+    
+}
